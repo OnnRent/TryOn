751963829b06b40b4bd76810d584580be659b1e3
OnnRent <vkarnwal8279@gmail.com>
2025-12-30 03:54:34 +0530

    removed await

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const { v4: uuidv4 } = await import('uuid');
+const { v4: uuidv4 } = require('uuid');
 const pool = require("./db");
 const multer = require("multer");
 const cors = require("cors");
